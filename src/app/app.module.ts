42f51b50745a63a778d6bb2a3ce2c53c69566d78
Gustavo Rosa <68282028+gustavodsrosa@users.noreply.github.com>
2023-10-03 21:24:04 -0300

    feature: Login page initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './modules/authentication/login-page/login-page.component';
+import { RouterModule, Routes } from '@angular/router';
+
+/**
+ * Adicionando as rotas ao projeto
+ */
+const routes : Routes = [
+  {
+    path: '',
+    component: LoginPageComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
